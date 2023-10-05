ce99a9697f39625e8cba9faaab3f9bd934d1f38d
risom <rich.john.isom@gmail.com>
2023-10-05 12:31:56 -0700

    Added eventtrigger for string implmentation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
+//import github from '../img/github-icon.svg'
 import logo from '../img/logo.png'
 
 const Navbar = () => {
