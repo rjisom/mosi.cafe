adf9a9161d7733e51a82ab9e584577cb9824ed2b
risom <rich.john.isom@gmail.com>
2023-10-10 11:21:42 -0700

    added eventrigger widget for testing

---
+++ src/data/menu-static.ts
@@ -0,0 +1,145 @@
+export const coffees = [
+    {
+        id: 1,
+        tags: ["traditional"],
+        name: "Drip Cofee",
+        description: "",
+        photo: "coff.png",
+        price: 1.9,
+        size: "S M L"
+      },
+      {
+        id: 2,
+        tags: ["traditional"],
+        name: "Traditional Tea",
+        description: "",
+        photo: "tea.png",
+        price: 1.9,
+        size: "S M L"
+      },
+    {
+      id: 3,
+      tags: ["traditional","has milk"],
+      name: "Milk",
+      description: "",
+      photo: "coff.png",
+      price: 1.9,
+      size: "S M L"
+    },
+    {
+      id: 4,
+      tags: ["traditional"],
+      name: "Expresso Americano",
+      description: "",
+      photo: "coff.png",
+      price: 1.9,
+      size: "S M L"
+    },
+    {
+      id: 5,
+      tags: ["special", "X-TRA"],
+      name: "Expresso XXX",
+      description: "",
+      photo: "coff.png",
+      price: 1.9,
+      size: "S"
+    },
+    {
+      id: 6,
+      tags: ["traditional", "cold"],
+      name: "Iced Coffee",
+      description: "",
+      photo: "coff.png",
+      price: 1.9,
+      size: "S M L"
+    },
+    {
+      id: 7,
+      tags: ["traditional", "has milk"],
+      name: "Café com Leite",
+      description: "",
+      photo: "coff.png",
+      price: 1.9,
+      size: "S M L"
+    },
+    {
+      id: 8,
+      tags: ["traditional", "has milk"],
+      name: "Latte",
+      description: "",
+      photo: "coff.png",
+      price: 1.9,
+      size: "S M L"
+    },
+    {
+      id: 9,
+      tags: ["traditional", "has milk"],
+      name: "Capuccino",
+      description: "",
+      photo: "coff.png",
+      price: 1.9,
+      size: "S M L"
+    },
+    {
+      id: 10,
+      tags: ["traditional", "has milk"],
+      name: "Macchiato",
+      description: "",
+      photo: "coff.png",
+      price: 1.9,
+      size: "S M L"
+    },
+    {
+      id: 11,
+      tags: ["traditional", "has milk"],
+      description: "",
+      photo: "coff.png",
+      price: 1.9,
+      size: "S M L"
+    },
+    {
+      id: 12,
+      tags: ["special"],
+      name: "Chocolate Milk",
+      description: "",
+      photo: "coff.png",
+      price: 4.9,
+      size: "S M"
+    },
+    {
+      id: 13,
+      tags: ["special"],
+      name: "Cubano",
+      description: "",
+      photo: "coff.png",
+      price: 1.9,
+      size: "S M L"
+    },
+    {
+      id: 14,
+      tags: ["special"],
+      name: "Espresso",
+      description: "",
+      photo: "coff-es.png",
+      price: 1.9,
+      size: "S"
+    },
+    {
+      id: 15,
+      tags: ["special"],
+      name: "Seasonal",
+      description: "",
+      photo: "coff.png",
+      price: 6.9,
+      size: "S M L"
+    },
+    {
+      id: 16,
+      tags: ["special"],
+      name: "Irish Creme Coffee",
+      description: "",
+      photo: "coff.png",
+      price: 4.9,
+      size: "S"
+    },
+  ];
